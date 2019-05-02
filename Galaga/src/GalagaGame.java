7628a7b08daf41c575eec7b66b8e745086be3251
USER <USER@AMP-05>
2019-05-02 12:51:03 +0900

    commit

---
+++ Galaga/src/GalagaGame.java
@@ -0,0 +1,71 @@
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GalagaGame extends JPanel implements KeyListener {
+
+	private boolean running = true;
+
+	private ArrayList sorites = new ArrayList();
+	private Sprite starship;
+
+	private BufferedImage alienImage;
+	private BufferedImage shotInage;
+	private BufferedImage shipImage;
+
+	public GalagaGame() {
+		JFrame frame = new JFrame("Galaga Game");
+
+		frame.setSize(800, 600);
+		frame.add(this);
+		frame.setResizable(false);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		try {
+			shotImage = ImageIO.read(new File("fire.pug"));
+			shipImage = ImageIO.read(new File("starship.png"));
+			alienImage = ImageIO.read(new File("alien.png"));
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		this.requestFocus();
+		this.initSprites();
+		addKeyListener(this);
+
+	}
+
+	private void initSprites() {
+		starship = new StarShipSprite(this, shipImage, 370, 550);
+		sprites.add(starship);
+		for (int y = 0; y < 5; y++) {
+			for (int x = 0; x < 12; x++) {
+				Sprite alien = new AlienSprite(this, alienImage, 100 + (x * 50), (50) + y * 30);
+				sprites.add(alien);
+			}
+		}
+	}
+
+	private void startGame() {
+		sprites.clear();
+		initSprites();
+	}
+	
+	public void endGame() {
+		System.exit(0);
+	}
+
+	public void removeSprite(Spi)
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
