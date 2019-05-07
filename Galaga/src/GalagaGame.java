1211a692cf3869b77a79734615d8f00de34a93f9
tlsgsutn <48539292+tlsgsutn@users.noreply.github.com>
2019-05-07 22:10:08 +0900

    Update GalagaGame.java

--- Galaga/src/GalagaGame.java
+++ Galaga/src/GalagaGame.java
@@ -1,3 +1,6 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -12,11 +15,11 @@ public class GalagaGame extends JPanel implements KeyListener {
 
 	private boolean running = true;
 
-	private ArrayList sorites = new ArrayList();
+	private ArrayList sprites = new ArrayList();
 	private Sprite starship;
 
 	private BufferedImage alienImage;
-	private BufferedImage shotInage;
+	private BufferedImage shotImage;
 	private BufferedImage shipImage;
 
 	public GalagaGame() {
@@ -29,7 +32,7 @@ public class GalagaGame extends JPanel implements KeyListener {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		try {
-			shotImage = ImageIO.read(new File("fire.pug"));
+			shotImage = ImageIO.read(new File("fire.png"));
 			shipImage = ImageIO.read(new File("starship.png"));
 			alienImage = ImageIO.read(new File("alien.png"));
 
@@ -57,15 +60,83 @@ public class GalagaGame extends JPanel implements KeyListener {
 		sprites.clear();
 		initSprites();
 	}
-	
+
 	public void endGame() {
 		System.exit(0);
 	}
 
-	public void removeSprite(Spi)
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public void removeSprite(Sprite sprite) {
+		sprites.remove(sprite);
 	}
 
+	public void fire() {
+		ShotSprite shot = new ShotSprite(this, shotImage, starship.getX() + 10, starship.getY() - 30);
+		sprites.add(shot);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.setColor(Color.black);
+		g.fillRect(0, 0, 800, 600);
+		for (int i = 0; i < sprites.size(); i++) {
+			Sprite sprite = (Sprite) sprites.get(i);
+			sprite.draw(g);
+		}
+	}
+
+	public void gameLoop() {
+
+		while (running) {
+			for (int i = 0; i < sprites.size(); i++) {
+				Sprite sprite = (Sprite) sprites.get(i);
+				sprite.move();
+			}
+
+			for (int p = 0; p < sprites.size(); p++) {
+				for (int s = p + 1; s < sprites.size(); s++) {
+					Sprite me = (Sprite) sprites.get(p);
+					Sprite other = (Sprite) sprites.get(s);
+
+					if (me.checkCollision(other)) {
+						me.handleCollision(other);
+						other.handleCollision(me);
+					}
+				}
+			}
+
+			repaint();
+			try {
+				Thread.sleep(10);
+			} catch (Exception e) {
+			}
+		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			starship.setDx(-3);
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			starship.setDx(+3);
+		if (e.getKeyCode() == KeyEvent.VK_SPACE)
+			fire();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			starship.setDx(0);
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			starship.setDx(0);
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+	}
+
+	public static void main(String argv[]) {
+		GalagaGame g = new GalagaGame();
+		g.gameLoop();
+	}
 }
