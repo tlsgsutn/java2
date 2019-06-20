1b207f673820dceb59338ab4f24150e22bad7efd
USER <USER@AMP-06>
2019-06-20 12:39:05 +0900

    commit

---
+++ kakaotalk/src/kakaotalk/CalculationClientFrame.java
@@ -0,0 +1,95 @@
+package kakaotalk;
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class CalculationClientFrame extends JFrame {
+	private JTextField startTf = new JTextField(7);
+	private JTextField operatorTf = new JTextField(3);
+	private JTextField endTf = new JTextField(7);
+	private JTextField resTf = new JTextField(7);
+	private JButton calcBtn = new JButton("계산");
+	private Socket socket = null;
+	private BufferedReader in = null;
+	private BufferedWriter out = null;
+
+	public CalculationClientFrame() {
+		super("계산 클라이언트");
+		setSize(410, 100);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		c.add(startTf);
+		c.add(operatorTf);
+		c.add(endTf);
+		c.add(new JLabel("="));
+		c.add(resTf);
+		c.add(calcBtn);
+
+		setVisible(true);
+		setupConnection();
+
+		calcBtn.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				try {
+					String startText = startTf.getText().trim();
+					String operatorText = operatorTf.getText().trim();
+					String endText = endTf.getText().trim();
+
+					if (startText.length() == 0 || operatorText.length() == 0 
+							||endText.length() == 0) 
+							return;
+					
+						out.write(startText + "\n");
+						out.write(operatorText + "\n");
+						out.write(endText + "\n");
+						out.flush();
+
+						String result = in.readLine();
+						resTf.setText(result);
+					
+				} catch (IOException e) {
+					// TODO: handle exception
+					System.out.println("클라이언트 : 서버로부터 연결 종료");
+					return;
+					// e.printStactTeace();
+				}
+			}
+		});
+	}
+
+	public void setupConnection() {
+		try {
+			socket = new Socket("localhost", 9998);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new CalculationClientFrame();
+	}
+
+}
---
+++ kakaotalk/src/kakaotalk/CalculationServerFrame.java
@@ -0,0 +1,131 @@
+package kakaotalk;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.Buffer;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class CalculationServerFrame extends JFrame {
+	private JTextArea log = new JTextArea();
+
+	public CalculationServerFrame() {
+		super("멀티 스레드 계산 서버");
+		setSize(250, 250);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		Container c = getContentPane();
+		c.add(new JLabel("계산 서버 입니디"));
+		c.add(new JScrollPane(log), BorderLayout.CENTER);
+		setVisible(true);
+
+		new ServerThread().start();
+	}
+
+	class ServerThread extends Thread {
+		@Override
+		public void run() {
+			ServerSocket listener = null;
+			Socket socket = null;
+			try {
+				listener = new ServerSocket(9998);
+				while (true) {
+					socket = listener.accept();
+					log.append("클라이언트 연결됨\n");
+					new ServiceThread(socket).start();
+
+				}
+			} catch (IOException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			try {
+				if (listener != null)
+					listener.close();
+				if (socket != null)
+					socket.close();
+			} catch (IOException e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+		}
+	}
+
+	class ServiceThread extends Thread {
+		private Socket socket = null;
+		private BufferedReader in = null;
+		private BufferedWriter out = null;
+
+		public ServiceThread(Socket socket) {
+			this.socket = socket;
+			try {
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+			} catch (IOException e) {
+				// TODO:	 handle exception
+				e.printStackTrace();
+			}
+		}
+		public void run(){
+			while(true){
+				try {
+					String first = in.readLine();
+					String operator = in.readLine();
+					String second = in.readLine();
+					int a = Integer.parseInt(first);
+					int b = Integer.parseInt(second);
+					
+					String resText = "";
+					int res = 0;
+				switch (operator) {
+				case "+" : res = a+b; resText = Integer.toString(res); break;
+					
+				case "-" : res = a-b; resText = Integer.toString(res); break;
+				
+				case "*" : res = a*b; resText = Integer.toString(res); break;
+				
+				case "/" : 
+					if(b==0)resText = "0으론 나눌수 없음";
+					else{
+						res = a/b;
+						resText = Integer.toString(res);
+						
+					
+					}	
+				break;
+				default:
+					resText = "잘못된 연산";
+				}
+				out.write(resText + "\n");
+				out.flush();
+				log.append(first + operator + second + "=" +resText);
+				
+				} catch (IOException e) {
+					log.append("연결종료\n");
+					System.out.println("연결 종료");
+					return;
+					//e.printStackTrace();
+					
+				}
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new CalculationServerFrame();
+	}
+
+}
